a81502876e52821c3f8990b2707ba44a16090456
Artur Minasian <Arthrmin@gmail.com>
2024-09-02 22:15:06 +0300

    HW2

--- hello world.py
+++ hello world.py
@@ -1,1 +1,19 @@
+import math
+from array import array
+
+
 print('Hello World')
+
+a = array('i',[0,1,2,3])
+a.reverse()
+a.insert(0,45)
+print(a)
+
+def sum (a,b):
+    a = math.pow(a,2)
+    return a+b
+print(sum(9,300))
+
+for greetings in range(len(a)):
+    print('Hello World')
+
