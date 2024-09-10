6504a69d73ea708730d1b3ef35206213e7a57492
Artur Minasian <Arthrmin@gmail.com>
2024-09-10 10:44:05 +0300

    HW4.1

--- L4/Task_4.1.py
+++ L4/Task_4.1.py
@@ -1,14 +1,20 @@
 # arr = [0, 1, 0, 12, 3] #-> [1, 12, 3, 0, 0]
-arr = [0] #-> [0]
+# arr = [0] #-> [0]
 # arr = [1, 0, 13, 0, 0, 0, 5] #-> [1, 13, 5, 0, 0, 0, 0]
-# arr = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0] #-> [9, 7, 31, 45, 45, 45, 96, 0, 0, 0, 0, 0, 0, 0]
+arr = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0] #-> [9, 7, 31, 45, 45, 45, 96, 0, 0, 0, 0, 0, 0, 0]
 
-result = []
-zeros = []
-for x in arr:
-    if x == 0:
-        zeros.append(x)
-    else:
-        result.append(x)
-result.extend(zeros)
-print(result)
+zeros = [0]*arr.count(0)
+arr = [v for v in arr if v != 0] + zeros
+print(arr)
+
+# ще одне рішення
+
+# result = []
+# zeros = []
+# for x in arr:
+#     if x == 0:
+#         zeros.append(x)
+#     else:
+#         result.append(x)
+# result.extend(zeros)
+# print(result)
--- hello world.py
+++ hello world.py
@@ -17,6 +17,5 @@ print(sum(9,300))
 for greetings in range(len(a)):
     print('Hello World')
 
-arr = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0] #-> [9, 7, 31, 45, 45, 45, 96, 0, 0, 0, 0, 0, 0, 0]
-arr = [v for v in arr if v != 0] + [0] * arr.count(0)
-print(arr)
+
+
