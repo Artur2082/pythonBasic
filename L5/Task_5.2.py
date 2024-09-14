29745234b9b2efb1f1efb3c5f07026c6b4e65687
Artur Minasian <Arthrmin@gmail.com>
2024-09-14 12:38:14 +0300

    HW5.2

---
+++ L5/Task_5.2.py
@@ -0,0 +1,23 @@
+cont = 'Y'
+while cont == 'Y':
+    a = int(input('Введіть число a :'))
+    b = int(input('Введіть число b :'))
+    c = input('Введіть дію +,-,* або / :')
+    if c == '+':
+        print(a, '+', b, '= ', a + b)
+    elif c == '-':
+        print(a, '-', b, '= ', a - b)
+    elif c == '*':
+        print(a, '*', b, '= ', a * b)
+    elif c == '/':
+        if b == 0:
+            print('На нуль ділити не можна ! ')
+        else:
+            print(a, '/', b, '= ', a / b)
+    cont = input('Continue ? ')
+    if cont != 'Y':
+        print('Exit')
+
+
+
+
