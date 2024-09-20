ba9fc7b3a2e42af462db2996af3dc78de63ccf5e
Artur Minasian <Arthrmin@gmail.com>
2024-09-20 10:18:17 +0300

    HW7.4

---
+++ L7/Task_7.4.py
@@ -0,0 +1,9 @@
+def common_elements():
+        set_a = set([v for v in range(100) if v %3 == 0])
+        set_b = set([v for v in range(100) if v %5 == 0])
+        common_set = set_a.intersection(set_b)
+        print(common_set)
+        return common_set
+
+assert common_elements() == {0, 75, 45, 15, 90, 60, 30}
+
