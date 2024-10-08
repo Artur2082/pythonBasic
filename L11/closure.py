fb958df98575cf02aafb4f1ca4ed8500d634b4d7
Artur Minasian <Arthrmin@gmail.com>
2024-10-08 23:40:55 +0300

    HW12.2

--- L11/closure.py
+++ L11/closure.py
@@ -1,7 +1,8 @@
 def closure_example(x):
     def inner_function(y):
-        number = y + x
-        return number
+        nonlocal x
+        x = x + y
+        return x
     return inner_function
 
 
@@ -11,7 +12,7 @@ test2 = closure_example(-2)
 print('test2 = ', test2(5))
 test3 = closure_example(10)
 print('test3 = ', test3(2))
-print('test3 = ', test3(5))  # тут результат має бути 17, а виходить 15
+print('test3 = ', test3(5))
 test4 = closure_example(-8)
 print('test4 = ', test4(10))
-print('test4 = ', test4(-4))  # тут результат має бути -2, а виходить -12
+print('test4 = ', test4(-4))
